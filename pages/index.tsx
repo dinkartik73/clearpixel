5cf5ca2b42e17f75294a7c676d4dfb7ab1ce236c
dinkartik73 <159926913+dinkartik73@users.noreply.github.com>
2024-02-29 01:26:44 +0530

    chore: ⚓ typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,39 +8,18 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Face Photo Restorer</title>
+        <title>Photo Restorer</title>
       </Head>
-      {/* <Header /> */}
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-20">
-        <a
-          href="https://twitter.com/nutlope/status/1704894145003741611"
-          target="_blank"
-          rel="noreferrer"
-          className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
-        >
-          Used by over <span className="font-semibold">470,000</span> happy
-          users
-        </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
           Restoring old photos for everyone.
         </h1>
 
         <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
-          Have old and blurry face photos? Let our AI restore them so those
+          Have old and blurry face photos? Let us restore them so those
           memories can live on. 100% free – restore your photos today.
         </p>
         <div className="flex justify-center space-x-4">
-          {/* <a
-            className="bg-white rounded-xl text-black font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-gray-100 border"
-            href="https://www.roomgpt.io/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button onClick={() => va.track("RoomGPT link clicked")}>
-              Check out roomGPT
-            </button>
-          </a> */}
-
           <Link
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
@@ -75,8 +54,6 @@ const Home: NextPage = () => {
           </div>
         </div>
       </main>
-      {/* <Testimonials /> */}
-      {/* <Footer /> */}
     </div>
   );
 };
